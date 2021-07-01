50c17e7824dfdc780ddb4dda13aa2f90f7fbb090
0x009922 <a.marcius26@gmail.com>
2021-07-01 21:17:18 +0300

    chore: remove garbage commented code

--- packages/enum/src/option.ts
+++ packages/enum/src/option.ts
@@ -4,30 +4,3 @@ export type Option<T> = Enum<{
     None: undefined;
     Some: Valuable<T>;
 }>;
-
-// export function Some<T>(value: T): Option<T> {
-//     return Enum.create('Some', value);
-// }
-
-// export function None<T>(): Option<T> {
-//     return Enum.create('None');
-// }
-
-// export class Option<T> extends Enum<{
-//     Some: { value: T };
-//     None: undefined;
-// }> {
-//     public static Some<T>(value: T): Option<T> {
-//         return new Option('Some', { value });
-//     }
-
-//     public static None<T>(): Option<T> {
-//         return new Option('None');
-//     }
-
-//     public unwrap(): T {
-//         return this.as('Some');
-//     }
-
-//     // TODO
-// }
