6cdf4e96149bb527a7982530e7d5d935dc1fdc0d
0x009922 <a.marcius26@gmail.com>
2021-07-01 20:40:51 +0300

    chore: remove json, rename package dirs back

--- packages/json-core/README.md
+++
@@ -1,6 +0,0 @@
-# json-core
-
-### Questions
-
--   What to do with numbers, big integers? How are they encoded?
--   Handle Option specially?
--- packages/json-core/jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    // transformIgnorePatterns: ['node_modules/(?!(.+hex-my-bytes)/)'],
-};
