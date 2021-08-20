9a11446623eeff3d33b93eda49028b009c4009dc
0x009922 <a.marcius26@gmail.com>
2021-08-20 19:39:54 +0300

    rm elixir codegen from repo

--- packages/namespace-codegen-next/lib/namespace_codegen.ex
+++
@@ -1,9 +0,0 @@
-defmodule NamespaceCodegen do
-  @moduledoc """
-  Documentation for `NamespaceCodegen`.
-  """
-
-  def generate_with_schema(schema) do
-    :unimplemented
-  end
-end
--- packages/namespace-codegen-next/lib/namespace_codegen/schema/scan.ex
+++
@@ -1,7 +0,0 @@
-defmodule NamespaceCodegen.Schema.Scan do
-  @doc """
-  Scans raw schema, finds any errors (in refs, in discriminants etc), used STD codecs
-  """
-  def scan_schema(schema) do
-  end
-end
