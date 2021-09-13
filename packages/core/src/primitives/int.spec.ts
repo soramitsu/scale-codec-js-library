63ad1421e353659447aaf1ccde12fb45234a91f0
0x009922 <a.marcius26@gmail.com>
2021-09-13 10:30:43 +0300

    chore: remove `.only` in test and dead code

--- packages/core/src/primitives/int.spec.ts
+++ packages/core/src/primitives/int.spec.ts
@@ -1,12 +1,5 @@
 import JSBI from 'jsbi';
-import {
-    AllowedBits,
-    BigIntCodecOptions,
-    decodeBigInt,
-    encodeBigInt,
-    // encodeBigIntBigEndian,
-    // encodeBigIntLittleEndian,
-} from './int';
+import { AllowedBits, BigIntCodecOptions, decodeBigInt, encodeBigInt } from './int';
 import ENCODE_TEST_DATA from '../../../rust-ints/output.json';
 
 function parseSpacedHexToBytes(hex: string): Uint8Array {
@@ -63,7 +56,7 @@ describe('int/uint', () => {
         });
     }
 
-    test.only('Uint8Array is reusable after decoding', () => {
+    test('Uint8Array is reusable after decoding', () => {
         // Arrange
         const num = JSBI.BigInt(-123);
         const opts: BigIntCodecOptions = {
@@ -99,89 +92,3 @@ describe('int/uint', () => {
         expect(firstResult).toEqual([num, 8]);
     });
 });
-
-// describe('encodeBigInt()', (): void => {
-//     it('converts null values to 0x00', (): void => {
-//         expect(encodeBigInt(JSBI.BigInt(0), { endianness: 'be' })).toEqual(new Uint8Array());
-//     });
-
-//     it('converts null values to 0x00000000 (bitLength)', (): void => {
-//         expect(encodeBigInt(JSBI.BigInt(0), { bits: 32, endianness: 'be' })).toEqual(new Uint8Array([0, 0, 0, 0]));
-//     });
-
-//     it('converts BN values to a prefixed hex representation', (): void => {
-//         expect(encodeBigInt(JSBI.BigInt(0x123456), { endianness: 'be' })).toEqual(new Uint8Array([0x12, 0x34, 0x56]));
-//     });
-
-//     it('converts BN values to a prefixed hex representation (bitLength)', (): void => {
-//         expect(encodeBigInt(JSBI.BigInt(0x123456), { bits: 32, endianness: 'be' })).toEqual(
-//             new Uint8Array([0x00, 0x12, 0x34, 0x56]),
-//         );
-//     });
-
-//     it('converts using little endian (as set)', (): void => {
-//         expect(encodeBigInt(JSBI.BigInt(0x123456), { bits: 32, endianness: 'le' })).toEqual(
-//             new Uint8Array([0x56, 0x34, 0x12, 0x00]),
-//         );
-//     });
-
-//     it('converts negative numbers', (): void => {
-//         expect(encodeBigInt(JSBI.BigInt(-1234), { isSigned: true })).toEqual(new Uint8Array([46, 251]));
-//     });
-
-//     it('converts negative numbers (BE)', (): void => {
-//         expect(encodeBigInt(JSBI.BigInt(-1234), { isSigned: true, endianness: 'be' })).toEqual(
-//             new Uint8Array([251, 46]),
-//         );
-//     });
-
-//     it('converts negative numbers (bitLength)', (): void => {
-//         expect(encodeBigInt(JSBI.BigInt(-1234), { bits: 32, isSigned: true })).toEqual(
-//             new Uint8Array([46, 251, 255, 255]),
-//         );
-//     });
-
-//     it('converts negative numbers (another)', () => {
-//         expect(encodeBigInt(JSBI.BigInt(-4123), { bits: 32, isSigned: true })).toEqual(
-//             new Uint8Array([229, 239, 255, 255]),
-//         );
-//     });
-// });
-
-// describe('decodeBigInt', (): void => {
-//     it('converts little-endian by default', (): void => {
-//         expect(decodeBigInt(new Uint8Array([0x12, 0x34]))[0].toString(16)).toBe('3412');
-//     });
-
-//     it('converts values (big-endian)', (): void => {
-//         expect(decodeBigInt(new Uint8Array([0x12, 0x34]), { endianness: 'be' })[0].toString(16)).toBe('1234');
-//     });
-
-//     it('converts values (little-endian)', (): void => {
-//         expect(decodeBigInt(new Uint8Array([0x12, 0x34]), { endianness: 'le' })[0].toString(16)).toBe('3412');
-//     });
-
-//     it('converts empty', (): void => {
-//         expect(decodeBigInt(new Uint8Array(), { endianness: 'le' })[0].toString(16)).toBe('0');
-//     });
-
-//     it('handles negative numbers (little-endian)', (): void => {
-//         expect(decodeBigInt(new Uint8Array([46, 251]), { endianness: 'le', isSigned: true })[0].toString()).toBe(
-//             '-1234',
-//         );
-//     });
-
-//     it('handles negative numbers (big-endian)', (): void => {
-//         expect(decodeBigInt(new Uint8Array([251, 46]), { endianness: 'be', isSigned: true })[0].toString()).toBe(
-//             '-1234',
-//         );
-//     });
-
-//     it('handles overflows correctly (little-endian)', (): void => {
-//         expect(decodeBigInt(new Uint8Array([0, 1, 0, 0, 0, 0, 0, 0]))[0].toString()).toBe('256');
-//     });
-
-//     it('takes only specified count of bits', () => {
-//         expect(decodeBigInt(new Uint8Array([0, 255]), { bits: 8 })[0].toString()).toBe('0');
-//     });
-// });
