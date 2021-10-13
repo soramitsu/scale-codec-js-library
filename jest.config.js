b9472d4f5e00e4f3d47fae87151b2b96f8e7ec31
0x009922 <a.marcius26@gmail.com>
2021-10-13 13:41:13 +0300

    test: apply exluding for jest properly

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,15 @@
 module.exports = {
-    // preset: 'ts-jest',
     testEnvironment: 'node',
     transform: {
         '^.+\\.ts$': ['esbuild-jest', { sourcemap: true }],
     },
-    testMatch: ['**/packages/**/*.spec.ts', '**/packages/**/__tests__/**/*.ts'],
+    testMatch: [
+        '**/packages/**/*.spec.ts',
+        '**/packages/**/__tests__/**/*.ts',
+        '!**/packages/namespace*/**',
+        '!**/*.d.ts',
+    ],
     roots: ['<rootDir>/packages'],
-    testPathIgnorePatterns: ['/node_modules/', '/dist/', 'packages/namespace*'],
+    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
     modulePathIgnorePatterns: ['/dist/'],
-    // moduleNameMapper: {
-    //     '^@scale-codec/(.*)$': '<rootDir>/packages/$1/src/lib.ts',
-    // },
 };
