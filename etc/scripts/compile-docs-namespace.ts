6c7b3f6ccfaafdbeaf1484db9dc1d744b5e93742
0x009922 <a.marcius26@gmail.com>
2022-05-30 12:02:25 +0300

    chore: remove old scripts

--- etc/scripts/compile-docs-namespace.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'fs/promises'
-import definition from '../../packages/docs/src/snippets/namespace-schema'
-import { renderNamespaceDefinition } from '../../packages/docs/node_modules/@scale-codec/definition-compiler/src/lib'
-import { DOCS_NAMESPACE_SCHEMA_COMPILED_SNIPPET_PATH as OUTPUT_FILE } from '../meta'
-
-export default async function () {
-  const content = renderNamespaceDefinition(definition)
-  await fs.writeFile(OUTPUT_FILE, content)
-}
